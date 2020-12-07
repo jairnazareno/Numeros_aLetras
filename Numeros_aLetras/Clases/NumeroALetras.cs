13ed3f1f9c8148c2d069deb3ecc9534fef8c5a1f
JAIR NAZARENO <jairnazareno12@gmail.com>
2020-12-07 09:43:23 -0500

    Agregar archivos de proyecto.

---
+++ Numeros_aLetras/Clases/NumeroALetras.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTILS
+{
+    class NumeroALetras
+    {
+      static  String[] Unidades = { "cero","uno","dos","tres","cuatro","cinco",
+                             "seis","siete","ocho","nueve","diez","once","doce","trece"
+                              ,"catorce","quince","dieciseis","diecisiete","dieciocho","diecinueve"};
+
+       static String[] Decenas = { "diez", "veinte", "treinta", "cuarenta", 
+                    "cincuenta", "sesenta", "setenta", "ochenta", "noventa" };
+
+       static String[] Centenas = {"cien","dosciento","trescientos","cuatroscientos","quinientos"
+                             ,"siescientos","setecientos","ochocientos","novecientos"};
+    
+        public static void testUnidades()
+        {
+            foreach (string cad in Unidades)
+            {
+                Console.WriteLine(cad);
+            }
+        }
+        /// <summary>
+        /// Devielve el equivalente en letras y unidades
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static string getUnidades(int num)
+        {
+            String aux = "";
+
+            if (num >= 0 && num < 20)
+                aux = Unidades[num];
+
+            return "";
+        }
+    }
+
+}
---
+++ Numeros_aLetras/FrmEmisionDeCheque.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Numeros_aLetras
+{
+    partial class FrmEmisionDeCheque
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPagueseAlaOrdenDe = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Paguese a la orden de :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "La cantidad de :";
+            // 
+            // txtPagueseAlaOrdenDe
+            // 
+            this.txtPagueseAlaOrdenDe.Location = new System.Drawing.Point(180, 42);
+            this.txtPagueseAlaOrdenDe.Name = "txtPagueseAlaOrdenDe";
+            this.txtPagueseAlaOrdenDe.Size = new System.Drawing.Size(266, 20);
+            this.txtPagueseAlaOrdenDe.TabIndex = 2;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(135, 81);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 3;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(39, 162);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResultado.Size = new System.Drawing.Size(462, 89);
+            this.txtResultado.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(265, 135);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // FrmEmisionDeCheque
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 286);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtPagueseAlaOrdenDe);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmEmisionDeCheque";
+            this.Text = "Emision De Cheque";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPagueseAlaOrdenDe;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
